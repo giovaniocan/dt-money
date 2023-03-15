97b284972caec868b8b0d24cb74068f94e2f9e39
giovani <giovaniocan@gmail.com>
2023-03-15 15:19:45 -0300

    css from Header is ended

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components'
+import { Transaction } from './pages/transactions'
 import { Globalstyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
@@ -6,7 +7,7 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Globalstyle />
-      hello World teste
+      <Transaction />
     </ThemeProvider>
   )
 }

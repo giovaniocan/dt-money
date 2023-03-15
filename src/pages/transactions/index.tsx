184836cd8b62605c03242d2f91013f851c5cf5a3
giovani <giovaniocan@gmail.com>
2023-03-15 16:31:37 -0300

    css table from Transaction is ended

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,11 +1,36 @@
 import { Header } from '../../components/Header'
 import { Summay } from '../../components/Summary'
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionTable,
+} from './styles'
 
 export function Transaction() {
   return (
     <div>
       <Header />
       <Summay />
+      <TransactionsContainer>
+        <TransactionTable>
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+            <td>
+              <PriceHighLight variante="income">R$ 12.000,00</PriceHighLight>
+            </td>
+            <td>Venda</td>
+            <td>15/03/2023</td>
+          </tr>
+          <tr>
+            <td width="50%">Hamburgue</td>
+            <td>
+              <PriceHighLight variante="outcome">- R$ 59,00</PriceHighLight>
+            </td>
+            <td>Alimentação</td>
+            <td>10/03/2023</td>
+          </tr>
+        </TransactionTable>
+      </TransactionsContainer>
     </div>
   )
 }
